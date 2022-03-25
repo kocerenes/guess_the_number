92312423b6d3a7f80270954f46b3cc96fa153fc7
kocerenes <eneskocerr41@gmail.com>
2022-03-25 19:47:54 +0300

    Create home page.

--- app/src/main/java/com/example/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.guessthenumber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.guessthenumber.ui.theme.GuessTheNumberTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             GuessTheNumberTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HomePage()
                 }
             }
         }
@@ -25,14 +34,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomePage() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Tahmin Oyunu", fontSize = 40.sp)
+        Image(
+            painter = painterResource(id = R.drawable.zar_resim),
+            contentDescription =""
+        )
+        Button(
+            onClick = { 
+                      
+            },
+            modifier = Modifier.size(250.dp,50.dp),
+        ) {
+            Text(text = "OYUNA BAŞLA")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GuessTheNumberTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
